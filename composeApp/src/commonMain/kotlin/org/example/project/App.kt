c970047c6064bd9261dd51d75eb7bc92fafab756
Douglas Motta <douglas1102rm@hotmail.com>
2025-05-25 21:09:54 +0100

    código até aula 6

---
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,27 @@
+package org.example.project
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.example.project.navigation.AppRoutes
+import org.example.project.ui.movies.MoviesListRoute
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        val navController = rememberNavController()
+        NavHost(navController, startDestination = AppRoutes.MoviesList) {
+            composable<AppRoutes.MoviesList> {
+                MoviesListRoute()
+            }
+
+            composable<AppRoutes.MovieDetails> {
+                // MovieDetailsScreen()
+            }
+        }
+    }
+}
